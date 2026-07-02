60cbe8309d733dfcc5a34d861487e4f3ee9f5b96
Lukas Skipper <Lukas@Skippies-MacBook.local>
2026-07-02 18:38:37 +1200

    Debug Supabase environment variables

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,6 +3,17 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
+console.log("SUPABASE URL:", supabaseUrl);
+console.log("SUPABASE KEY:", supabaseAnonKey);
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error(
+    `Missing Supabase environment variables.
+URL: ${supabaseUrl}
+KEY: ${supabaseAnonKey ? "Present" : "Missing"}`
+  );
+}
+
 export const supabase = createClient(
   supabaseUrl,
   supabaseAnonKey
